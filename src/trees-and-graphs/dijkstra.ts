53b2c743c866fef673abc448ced9c8e5dc9d67f5
galkahana <gal.kahana@hotmail.com>
2022-11-28 06:14:04 +0200

    improve disjkstra implementation by replacing decreaseItem with a faster enque. added visit test to also drop revisiting

--- src/trees-and-graphs/dijkstra.ts
+++ src/trees-and-graphs/dijkstra.ts
@@ -31,6 +31,7 @@ export function buildGraph(vertices: string[], edges: {from:string, to:string, w
 export function dijsktra(graph: Graph, source: string) {
     const dist: Record<string, number> = {}
     const prev: Record<string, string> = {}
+    const visited: Set<string> = new Set<string>()
     const queue = new PriorityQueue<GraphNode>()
 
     for(const [ vertex, vertexNode ] of Object.entries(graph.vertices)) {
@@ -41,16 +42,19 @@ export function dijsktra(graph: Graph, source: string) {
 
     while(!queue.isEmpty()) {
         const visitedVertexNode = queue.extractMin()
-        const distanceToMe = dist[visitedVertexNode.data.vertex]
-        for(const neighbor of visitedVertexNode.data.neighbors) {
-            const alternativeWeight = distanceToMe + neighbor.weight
-            if(alternativeWeight < dist[neighbor.to.vertex]) { // note that this CANT be true to vertices already visited in and so NOT in the queue
-                dist[neighbor.to.vertex] = alternativeWeight
-                prev[neighbor.to.vertex] = visitedVertexNode.data.vertex
-                queue.decreaseItem(neighbor.to, alternativeWeight) // therefore this cannot throw
+        if(!visited.has(visitedVertexNode.data.vertex)) {
+            visited.add(visitedVertexNode.data.vertex)
+            const distanceToMe = dist[visitedVertexNode.data.vertex]
+            for(const neighbor of visitedVertexNode.data.neighbors) {
+                const alternativeWeight = distanceToMe + neighbor.weight
+                if(alternativeWeight < dist[neighbor.to.vertex]) {
+                    dist[neighbor.to.vertex] = alternativeWeight
+                    prev[neighbor.to.vertex] = visitedVertexNode.data.vertex
+                    // enqueue with new weight...which should be faster than finding the item and decreasing its value (as in queue.decreaseItem)
+                    queue.insert({ value: alternativeWeight, data: neighbor.to })
+                }
             }
-        }
-        
+        }        
     }
     return { dist, prev }
 }
